6fd7f0c0e2d61c23845eccc2141a99c100317e04
Douile <25043847+Douile@users.noreply.github.com>
2021-04-30 12:34:20 +0100

    Lint with shellcheck

--- build.sh
+++ build.sh
@@ -1,27 +1,27 @@
 #!/usr/bin/env bash
 
 info() {
-  echo "::info $@" >&2
+  echo "::info $*" >&2
 }
 
 error() {
-  echo "::error file=entrypoint.sh:: $@" >&2
+  echo "::error file=entrypoint.sh:: $*" >&2
 }
 
 set -eux
 PROJECT_ROOT="/rust/build/${GITHUB_REPOSITORY}"
 OUTPUT_DIR="$1"
 
-mkdir -p $PROJECT_ROOT
-rmdir $PROJECT_ROOT
-ln -s $GITHUB_WORKSPACE $PROJECT_ROOT
-cd $PROJECT_ROOT
+mkdir -p "$PROJECT_ROOT"
+rmdir "$PROJECT_ROOT"
+ln -s "$GITHUB_WORKSPACE" "$PROJECT_ROOT"
+cd "$PROJECT_ROOT"
 
 if [ -z "${SRC_DIR+0}" ]; then
   info "No SRC_DIR is set, using repo base dir"
 else
   info "Switching to src dir \"$SRC_DIR\""
-  cd $SRC_DIR
+  cd "$SRC_DIR"
 fi
 
 info "Installing additional linkers"
@@ -58,20 +58,21 @@ for BINARY in $BINARIES; do
   info "Building $BINARY..."
 
   if [ -x "./build.sh" ]; then
-    OUTPUT=`./build.sh "${CMD_PATH}" "${OUTPUT_DIR}"`
+    OUTPUT=$(./build.sh "${CMD_PATH}" "${OUTPUT_DIR}")
   else
     rustup target add "$RUSTTARGET"
     OPENSSL_LIB_DIR=/usr/lib64 OPENSSL_INCLUDE_DIR=/usr/include/openssl cargo build --release --target "$RUSTTARGET" --bin "$BINARY"
-    OUTPUT=$(find "target/${RUSTTARGET}/release/" -maxdepth 1 -type f -executable \( -name "${BINARY}" -o -name "${BINARY}.*" \) | tr "\n" " ")
+    OUTPUT=$(find "target/${RUSTTARGET}/release/" -maxdepth 1 -type f -executable \( -name "${BINARY}" -o -name "${BINARY}.*" \) -print0 | xargs -0)
   fi
 
   info "Saving $OUTPUT..."
 
+  # shellcheck disable=SC2086
   mv $OUTPUT "$OUTPUT_DIR" || error "Unable to copy binary"
 
   OUTPUT_LIST=""
   for f in $OUTPUT; do
-    OUTPUT_LIST="$OUTPUT_LIST $(basename $f)"
+    OUTPUT_LIST="$OUTPUT_LIST $(basename "$f")"
   done
 done
 echo "$OUTPUT_LIST"
