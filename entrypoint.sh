ee331f3908a13e7ea5f3ffde67ca91452e4eca35
Douile <25043847+Douile@users.noreply.github.com>
2021-04-30 12:39:38 +0100

    Lint with shellcheck

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,25 +11,25 @@ mkdir -p "$OUTPUT_DIR"
 
 FILE_LIST=$(/build.sh "$OUTPUT_DIR")
 
-EVENT_DATA=$(cat $GITHUB_EVENT_PATH)
-echo $EVENT_DATA | jq .
-UPLOAD_URL=$(echo $EVENT_DATA | jq -r .release.upload_url)
+EVENT_DATA=$(cat "$GITHUB_EVENT_PATH")
+echo "$EVENT_DATA" | jq .
+UPLOAD_URL=$(echo "$EVENT_DATA" | jq -r .release.upload_url)
 UPLOAD_URL=${UPLOAD_URL/\{?name,label\}/}
-RELEASE_NAME=$(echo $EVENT_DATA | jq -r .release.tag_name)
-PROJECT_NAME=$(basename $GITHUB_REPOSITORY)
+RELEASE_NAME=$(echo "$EVENT_DATA" | jq -r .release.tag_name)
+PROJECT_NAME=$(basename "$GITHUB_REPOSITORY")
 NAME="${NAME:-${PROJECT_NAME}_${RELEASE_NAME}}_${RUSTTARGET}"
 
 if [ -z "${EXTRA_FILES+x}" ]; then
   echo "::warning file=entrypoint.sh::EXTRA_FILES not set"
 else
-  for file in "$(echo -n "${EXTRA_FILES}" | tr " " "\n")"; do
+  for file in $(echo -n "${EXTRA_FILES}" | tr " " "\n"); do
     cp "$file" "$OUTPUT_DIR"
   done
 fi
 
 cd "$OUTPUT_DIR"
 
-if [ ! -z "${EXTRA_FILES+0}" ]; then
+if [ -n "${EXTRA_FILES+0}" ]; then
   FILE_LIST="${FILE_LIST} ${EXTRA_FILES}"
 fi
 
@@ -37,6 +37,7 @@ FILE_LIST=$(echo "${FILE_LIST}" | awk '{$1=$1};1')
 
 ARCHIVE="tmp.zip"
 echo "::info Packing files: $FILE_LIST"
+# shellcheck disable=SC2086
 zip -9r $ARCHIVE ${FILE_LIST}
 
 CHECKSUM=$(sha256sum ${ARCHIVE} | cut -d ' ' -f 1)
@@ -50,7 +51,7 @@ curl \
 
 curl \
   -X POST \
-  --data $CHECKSUM \
+  --data "$CHECKSUM" \
   -H 'Content-Type: text/plain' \
   -H "Authorization: Bearer ${GITHUB_TOKEN}" \
   "${UPLOAD_URL}?name=${NAME}.sha256sum"
