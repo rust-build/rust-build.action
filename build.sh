796a6da3f27a6b9afa11b7c33ac4f2d90ae01392
Douile <25043847+Douile@users.noreply.github.com>
2022-01-19 05:14:06 +0000

    Allow the user to specify a TOOLCHAIN_VERSION via environment variable
    This allows specifying the rust version to be used (as well as nightly
    versions) when calling the action.
    
    Closes #35

--- build.sh
+++ build.sh
@@ -65,6 +65,14 @@ exit 1
 ;;
 esac
 
+info "Setting up toolchain"
+TOOLCHAIN_VERSION="${TOOLCHAIN_VERSION:-""}"
+if [ "$TOOLCHAIN_VERSION" != "" ]; then
+  rustup default "$TOOLCHAIN_VERSION"
+fi
+rustup target add "$RUSTTARGET"
+
+
 BINARIES="$(cargo read-manifest | jq -r ".targets[] | select(.kind[] | contains(\"bin\")) | .name")"
 
 OUTPUT_LIST=""
@@ -74,7 +82,6 @@ for BINARY in $BINARIES; do
   if [ -x "./build.sh" ]; then
     OUTPUT=$(./build.sh "${CMD_PATH}" "${OUTPUT_DIR}")
   else
-    rustup target add "$RUSTTARGET"
     OPENSSL_LIB_DIR=/usr/lib OPENSSL_INCLUDE_DIR=/usr/include/openssl CARGO_TARGET_DIR="./target" cargo build --release --target "$RUSTTARGET" --bin "$BINARY"
     OUTPUT=$(find "target/${RUSTTARGET}/release/" -maxdepth 1 -type f -executable \( -name "${BINARY}" -o -name "${BINARY}.*" \) -print0 | xargs -0)
   fi
